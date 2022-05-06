2369b6b5a4b65430f863ab4b6ee5742162c12a42
Biman Raj Shrestha <51041395+BimanShrestha@users.noreply.github.com>
2022-05-06 15:08:43 +0545

    basic command

---
+++ 1.py
@@ -0,0 +1,11 @@
+#This is a python progrm to find greatest number between 3 numberss
+a=input("enter 1 number: \n")
+b=input("enter 2 number: \n") 
+c=input("enter 3 number: \n") 
+num1=int(a)
+num2=int(b)
+num3=int(c) 
+
+if(num1>num2 or num1>num3):
+    print("Number 1 is great")
+
