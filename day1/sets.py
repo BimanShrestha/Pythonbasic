e860c37b091a1aeb218fbb55c2c3c03fe6024769
Biman Raj Shrestha <51041395+BimanShrestha@users.noreply.github.com>
2022-05-09 15:31:20 +0545

    updated string set and function

---
+++ day1/sets.py
@@ -0,0 +1,47 @@
+# Different types of sets in Python
+# set of integers
+my_set = {1, 2, 3}
+print(my_set)
+
+# set of mixed datatypes
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+# Distinguish set and dictionary while creating empty set
+
+# initialize a with {}
+a = {}
+
+# check data type of a
+print(type(a))
+
+# initialize a with set()
+a = set()
+
+# check data type of a
+print(type(a))
+
+# Set union method
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use | operator
+# Output: {1, 2, 3, 4, 5, 6, 7, 8}
+print(A | B)
+# Intersection of sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use & operator
+# Output: {4, 5}
+print(A & B)
+# Difference of two sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use - operator on A
+# Output: {1, 2, 3}
+print(A - B)
