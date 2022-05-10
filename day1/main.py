8c228fe05f805bb71d8d8e749cb660537a54c7a7
Biman Raj Shrestha <51041395+BimanShrestha@users.noreply.github.com>
2022-05-10 11:28:39 +0545

    updated class and varidable

--- day1/main.py
+++ day1/main.py
@@ -1,10 +1,18 @@
 class Employee:
+    increment =1.5
+    no_of_employee=0
     def __init__(self, fname, lname, salary):
         self.fname =fname
         self.lname =lname
         self.salary =salary
-
+        Employee.no_of_employee +=1
+    def increase(self):
+        self.salary=self.salary *Employee.increment
+print(Employee.no_of_employee)
 ram = Employee('ram','bahadur', 44000)
 sam = Employee('sam','bahadur', 44000)
-
-print(ram.fname, sam.fname)
+print(Employee.no_of_employee)
+#print(ram.fname, sam.fname)
+#print(Employee.__dict__)
+#print(ram.__dict__)
+#print(sam.__dict__)
